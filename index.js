b29c5f8bf70c0e84453ba10d51679b8f11eec930
Amanda <main@Amandas-MacBook-Air.local>
2020-06-23 21:30:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+  let thankYouCards = []
+  for (let counter = 0; counter < array.length; counter++ ) {
+    thankYouCards.push(`Thank you, ${array[counter]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards
+}
+
+
+function countDown(num) {
+  while (num > 0) {
+    console.log(num);
+    num -= 1;
+  }
+  console.log(num);  
+}
